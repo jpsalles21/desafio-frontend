dbc88cabf87295be9bf4d649265ed0b90d200c0f
jpsalles21 <joaopedrosalles.dev@gmail.com>
2024-07-13 23:51:33 -0300

    fix: change type name to variant

--- src/components/Button/button.tsx
+++ src/components/Button/button.tsx
@@ -1,18 +1,20 @@
 import './style.css'
 
 interface ButtonProps {
-    onClick?: () => any;
+    onClick?: (value: any) => any;
     label: string
-    type: 'login' | 'addUser' | 'save' | 'back';
+    variant: 'login' | 'addUser' | 'save' | 'back';
     width?: string
     height?: string
     disabled?: boolean;
+    type?: "button" | "submit" | "reset" | undefined
 }
-export const Button = ({ onClick, label, type, width, height, disabled = false }: ButtonProps) => {
+export const Button = ({ onClick, label, variant, width, height, disabled = false, type }: ButtonProps) => {
     return (
         <button
-            className={type}
+            className={variant}
             onClick={onClick}
+            type={type}
             style={{ width: `${width}`, height: `${height}` }}
         >
             {label}
