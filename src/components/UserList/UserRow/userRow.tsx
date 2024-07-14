880f50066aaf561e1f9554fd3b7890011d8f9fdd
jpsalles21 <joaopedrosalles.dev@gmail.com>
2024-07-13 23:53:30 -0300

    feat: add user list crud and styles

--- src/components/UserList/UserRow/userRow.tsx
+++ src/components/UserList/UserRow/userRow.tsx
@@ -1,17 +1,37 @@
 import { User } from "@/types/user";
+import './style.css'
 
 interface UserRowProps {
-    user: User
+    user: User,
+    openEditModal: () => void,
+    setSelectedUser: (user: User) => void
+    deleteUser: (user: User) => void  
+  
 }
-const UserRow = ({user}: UserRowProps) => (
+const UserRow = ({ user, openEditModal, setSelectedUser, deleteUser}: UserRowProps) => {
+    
+    const handleSelectedUser = () => { 
+        setSelectedUser(user);
+        openEditModal();
+    }    
+
+    const handleDeleteUser = () => {
+        deleteUser(user)
+    }
+
+    return (
     <tr>
         <td>{user.name}</td>
-        <td>{user.email}</td>
-        <td>{user.level}</td>
-        <td>           
-            
+        <td className="user-list-content__cell-alignment">{user.email}</td>
+        <td className="user-list-content__cell-alignment">{user.accessLevel}</td>
+        <td>
+            <div className="actions">
+                <img src="/assets/trash-2.svg" alt="trash" onClick={handleDeleteUser} />
+                <img src="/assets/edit.svg" alt="edit" onClick={handleSelectedUser} />
+            </div>
         </td>
     </tr>
 );
+}
 
 export default UserRow;
--- src/components/UserList/userList.tsx
+++ src/components/UserList/userList.tsx
@@ -1,34 +1,89 @@
-import { Button } from "../Button/button"
-import './style.css'
-import UserRow from "./UserRow/userRow"
-import { User } from "@/types/user"
+'use client'
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Button } from '../Button/button';
+import './style.css';
+import UserRow from './UserRow/userRow';
+import { User } from "@/types/user";
+import { UserModal } from '../UserModal/userModal';
 
-interface UserTableProps {
-    users: User[];
-}
+export const UserList = () => {
+    const [users, setUsers] = useState<User[]>([]);
+    const [selectedUser, setSelectedUser] = useState<User>();
+    const [loading, setLoading] = useState<boolean>(true);
+    const [error, setError] = useState<string | null>(null);
+    const [showModal, setShowModal] = useState<boolean>(false);
+    const [formType, setFormType] = useState<'create' | 'edit'>('create');
 
-export const UserList = ({ users }: UserTableProps) => {
+    useEffect(() => {
+        const fetchUsers = async () => {
+            try {
+                const response = await axios.get<User[]>('http://localhost:8080/users');
+                setUsers(response.data);
+            } catch (error) {
+                setError('Erro ao buscar usuários');
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchUsers();
+    }, [users]);
+
+    if (loading) {
+        return <div style={{ fontSize: '50px' }}>Carregando...</div>;
+    }
+
+    if (error) {
+        return <div>{error}</div>;
+    }
+
+    const handleAddNewUser = () => {
+        setShowModal(true);
+        setFormType('create');       
+    }
+
+    const handleEditUser = () => {
+        setShowModal(true);
+        setFormType('edit');
+    }
+
+    const handleCloseModal = () => { 
+         setShowModal(false)
+         setSelectedUser(undefined);
+    }
+
+    const deleteUser = async (user: User) => {
+        try {
+            const response = await axios.delete(`http://localhost:8080/user/${user.id}`)
+        } catch (error) {
+            console.log('Erro')
+        }
+    }
+
     return (
-        <section className="user-list">
-            <div className="user-list__header">
+        <section className="user-list-content">
+            <div className="user-list-content__header">
                 <h2>Usuários</h2>
-                <Button label="Adicionar usuário" type="addUser" />
+                <Button label="Adicionar usuário" variant="addUser" onClick={handleAddNewUser} />
             </div>
             <table>
                 <thead>
                     <tr>
-                        <th>Nome</th>
-                        <th>Email</th>
-                        <th>Nível de Acesso</th>
-                        <th>Ações</th>
+                        <th>NOME</th>
+                        
+                        <th className='user-list-content__cell-alignment'>EMAIL</th>
+                        <th className='user-list-content__cell-alignment'>NÍVEL DE ACESSO</th>
+                        <th></th>
                     </tr>
                 </thead>
                 <tbody>
                     {users.map(user => (
-                        <UserRow key={user.email} user={user} />
+                        <UserRow key={user.email} user={user} openEditModal={handleEditUser} setSelectedUser={setSelectedUser} deleteUser={deleteUser}/>
                     ))}
                 </tbody>
             </table>
+            <UserModal isOpen={showModal} closeModal={handleCloseModal}  selectedUser={selectedUser} formType={formType}/>
         </section>
-    )
-}
+    );
+};
