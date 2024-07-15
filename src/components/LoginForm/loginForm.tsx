08cc489fce7d557227981a1d0b30d24259ca1016
jpsalles21 <joaopedrosalles.dev@gmail.com>
2024-07-15 12:58:19 -0300

    fix: button variant

--- src/components/LoginForm/loginForm.tsx
+++ src/components/LoginForm/loginForm.tsx
@@ -12,19 +12,19 @@ export const LoginForm = () => {
 
         <div className="login-form__container">
             <h3>Bem vindo</h3>
-            <div className="login-form__inputs">
+            <form className="login-form__inputs">
                 <div className="login-form__input-email">
                     <label htmlFor="email">Email</label>
                     <input type="text" id="email" placeholder="Email" onChange={(e) => setEmailValue(e.target.value)} value={emailValue} />
                 </div>
                 <div className="login-form__input-password">
-                    <label htmlFor="password">Password</label>
-                    <input type='password' id="password" placeholder='Password' onChange={(e) => setPasswordValue(e.target.value)} value={passwordValue} />
+                    <label htmlFor="password">Senha</label>
+                    <input type='password' id="password" placeholder='Senha' onChange={(e) => setPasswordValue(e.target.value)} value={passwordValue} />
                 </div>
-            </div>
-            <div className="login-form__button">
-                <Button label="Login" type="login" />
-            </div>
+                <div className="login-form__button">
+                    <Button label="Login" variant="login" />
+                </div>
+            </form>
         </div>
 
     )
