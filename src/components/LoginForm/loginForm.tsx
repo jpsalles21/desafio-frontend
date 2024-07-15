02db0b7f46712a342b4a17a72c0117dc11e9b141
jpsalles21 <j.salles001@edu.pucrs.br>
2024-07-15 19:48:32 -0300

    fix: router push bolsistas

--- src/components/LoginForm/loginForm.tsx
+++ src/components/LoginForm/loginForm.tsx
@@ -1,19 +1,25 @@
 'use client'
 import './style.css'
 import { Button } from "@/components/Button/button";
+import { redirect, useRouter } from 'next/navigation';
 import { useState } from 'react'
 
 export const LoginForm = () => {
+    const router = useRouter();
 
     const [emailValue, setEmailValue] = useState('');
     const [passwordValue, setPasswordValue] = useState('');
+
+    const handleLogin = () => {
+        router.push('/bolsistas');
+    }
 
     return (
 
         <div className="login-form__container">
             <h3>Bem vindo</h3>
             <form className="login-form__inputs">
-                <div className="login-form__input-email">
+                <div className="login-form__input-email" >
                     <label htmlFor="email">Email</label>
                     <input type="text" id="email" placeholder="Email" onChange={(e) => setEmailValue(e.target.value)} value={emailValue} />
                 </div>
@@ -21,10 +27,10 @@ export const LoginForm = () => {
                     <label htmlFor="password">Senha</label>
                     <input type='password' id="password" placeholder='Senha' onChange={(e) => setPasswordValue(e.target.value)} value={passwordValue} />
                 </div>
-                <div className="login-form__button">
-                    <Button label="Login" variant="login" />
-                </div>
             </form>
+                <div className="login-form__button">
+                    <Button label="Login" variant="login" onClick={handleLogin} />
+                </div>
         </div>
 
     )
