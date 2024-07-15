e677152cc58682d14c7037e662faa6e740c6052b
jpsalles21 <joaopedrosalles.dev@gmail.com>
2024-07-15 12:57:18 -0300

    feat: add images and change styles

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -5,8 +5,9 @@ import { LoginForm } from "@/components/LoginForm/loginForm";
 export default function Login () {
     return (
         <div className="login-container">
-            <div className="art">
-                <p>oi</p>
+            <div className="login-container__art">
+                <img className="wave1" src="/assets/wave1.png" alt="" />
+                <img className="wave2"src="/assets/wave2.png" alt="" />
             </div>
             <section className="form-container">
               <LoginForm/>
